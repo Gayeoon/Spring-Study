f917e8bcd35c9f4b05e21feacd0e7911c1e754a3
Gayeoon <rkdus1177@naver.com>
2021-01-21 16:00:13 +0900

    make repository

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -14,11 +14,11 @@ public class HelloController {
         return "hello";
     }
 
-        @GetMapping("hello-mvc")
-        public String helloMvc(@RequestParam("name") String name, Model model) {
-            model.addAttribute("name", name);
-            return "hello-template";
-        }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 
     @GetMapping("hello-string")
     @ResponseBody
@@ -33,11 +33,14 @@ public class HelloController {
         hello.setName(name);
         return hello;
     }
+
     static class Hello {
         private String name;
+
         public String getName() {
             return name;
         }
+
         public void setName(String name) {
             this.name = name;
         }
